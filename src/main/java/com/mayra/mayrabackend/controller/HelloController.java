156c07fc31eb89ab488ecc7a2f75a9aee6a6097e
Hamidov Akbar <akbar.hamidov.02@gmail.com>
2023-12-01 13:15:00 +0100

    Packages Organized

--- src/main/java/com/mayra/mayrabackend/controller/HelloController.java
+++ src/main/java/com/mayra/mayrabackend/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.mayra.mayrabackend;
+package com.mayra.mayrabackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
