910c666e38e87be38e1021437377072b12336604
Hamidov Akbar <akbar.hamidov.02@gmail.com>
2023-12-02 15:36:19 +0100

    Init.sql added

---
+++ src/main/resources/init.sql
@@ -0,0 +1,8 @@
+-- User initialization
+INSERT INTO users (name, email, username, address, phone_number) VALUES ('John Doe', 'john@example.com', 'johndoe', '1234 Main St', '123-456-7890');
+
+-- Product initialization
+INSERT INTO product (name, description, price, stock_count, product_type) VALUES ('Sample Toy', 'A fun toy', 10.99, 100, 'TOY');
+INSERT INTO product (name, description, price, stock_count, product_type) VALUES ('Sample Cloth', 'A stylish cloth', 15.99, 50, 'CLOTH');
+
+-- Add more initialization data as needed
